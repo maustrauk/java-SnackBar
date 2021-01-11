ec19fe1473812e4830c1f2e522c40ddbab9d47ce
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-01-11 14:28:39 -0500

    Creating Customer Class

---
+++ src/snackBar/Customer.java
@@ -0,0 +1,42 @@
+package snackBar;
+
+public class Customer {
+    private static int maxId = 0;
+    private int id;
+    private String name;
+    private double cashOnHand;
+
+    public Customer (
+        String name,
+        double cash
+    ) {
+        maxId++;
+        id = maxId;
+        this.name = name;
+        this.cashOnHand = cash;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCashOnHand() {
+        return cashOnHand;
+    }
+
+    public void addToCashOnHand(double cash) {
+        this.cashOnHand = this.cashOnHand + cash;
+    }
+
+    public void buySnacks(double cost) {
+        this.cashOnHand = this.cashOnHand - cost;
+    }
+}
--- src/snackBar/Main.java
+++ src/snackBar/Main.java
@@ -2,6 +2,8 @@ package snackBar;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        System.out.println("My Snack Bar Project");
+        Customer person1 = new Customer("David", 50);
+        System.out.println("Name:" + person1.getName());
     }
 }
